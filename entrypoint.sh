04429d7a343707fa562c90575c328b50574a8cf9
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-03-31 19:02:54 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+# Define UUID and masquerade path, please modify it yourself. (Note: The masquerading path starts with / symbol, in order to avoid unnecessary trouble, please do not use special symbols.)
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
@@ -9,13 +9,13 @@ SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
 
-# 设置 nginx 伪装站
+# Set nginx masquerade station
 rm -rf /usr/share/nginx/*
 wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
 unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
 rm -f /usr/share/nginx/mikutap.zip
 
-# 伪装 xray 执行文件
+# Fake xray executable file
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv xray ${RELEASE_RANDOMNESS}
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
@@ -23,10 +23,10 @@ wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/ge
 cat config.json | base64 > config
 rm -f config.json
 
-# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
+# If there are three variables set for the Nezha probe, it will be installed. If not filled or incomplete, it will not be installed
 [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
 
-# 启用 Argo，并输出节点日志
+# Enable Argo, and output node logs
 cloudflared tunnel --url http://localhost:80 --no-autoupdate > argo.log 2>&1 &
 sleep 5 && argo_url=$(cat argo.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##")
 
@@ -61,40 +61,40 @@ cat > /usr/share/nginx/html/$UUID.html<<-EOF
 </head>
 <body bgcolor="#FFFFFF" text="#000000">
     <div>
-        <font color="#009900"><b>VMESS协议链接：</b></font>
+        <font color="#009900"><b>VMESS protocol link：</b></font>
     </div>
     <div>$vmlink</div>
     <div>
-        <font color="#009900"><b>VMESS协议二维码：</b></font>
+        <font color="#009900"><b>VMESS protocol QR code：</b></font>
     </div>
     <div><img src="/M$UUID.png"></div>
     <div>
-        <font color="#009900"><b>VLESS协议链接：</b></font>
+        <font color="#009900"><b>VLESS protocol link：</b></font>
     </div>
     <div>$vllink</div>
     <div>
-        <font color="#009900"><b>VLESS协议二维码：</b></font>
+        <font color="#009900"><b>VLESS protocol QR code：</b></font>
     </div>
     <div><img src="/L$UUID.png"></div>
     <div>
-        <font color="#009900"><b>TROJAN协议链接：</b></font>
+        <font color="#009900"><b>TROJAN protocol link：</b></font>
     </div>
     <div>$trlink</div>
     <div>
-        <font color="#009900"><b>TROJAN协议二维码：</b></font>
+        <font color="#009900"><b>TROJAN protocol QR code：</b></font>
     </div>
     <div><img src="/T$UUID.png"></div>
     <div>
-        <font color="#009900"><b>SS协议明文：</b></font>
+        <font color="#009900"><b>SS protocol plain text：</b></font>
     </div>
-    <div>服务器地址：$argo_url</div>
-    <div>端口：443</div>
-    <div>密码：$UUID</div>
-    <div>加密方式：chacha20-ietf-poly1305</div>
-    <div>传输协议：ws</div>
+    <div>server address：$argo_url</div>
+    <div>port：443</div>
+    <div>password：$UUID</div>
+    <div>Encryption：chacha20-ietf-poly1305</div>
+    <div>Transfer Protocol：ws</div>
     <div>host：$argo_url</div>
-    <div>path路径：$SS_WSPATH?ed=2048</div>
-    <div>TLS：开启</div>
+    <div>path：$SS_WSPATH?ed=2048</div>
+    <div>TLS：turn on</div>
 </body>
 </html>
 EOF
